f31b2b51b080a1d5d8a60752a7e222111f30b674
itzkanasama <164613153+itzkanasama@users.noreply.github.com>
2025-06-06 03:35:43 +0000

    touchscreen: td4310_e7/lansi: changes "-mi8953" suffix to "-vince"
    [RennAlt] i don't plan on bringing vince to unified right now
    
    Co-authored-by: RennAlt <shenprjktplayground@gmail.com>

--- drivers/input/touchscreen/td4310_e7/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/td4310_e7/synaptics_dsx_i2c.c
@@ -597,7 +597,7 @@ MODULE_DEVICE_TABLE(i2c, synaptics_rmi4_id_table);
 #ifdef CONFIG_OF
 static struct of_device_id synaptics_rmi4_of_match_table[] = {
 	{
-		.compatible = "td4310_mi8953-i2c",
+		.compatible = "td4310-vince-i2c",
 	},
 	{},
 };
